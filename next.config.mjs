127b6c4712b64df84374dc3015215679f8600c95
Alfredo Ingraldo <alfredo.ingraldo.u@gmail.com>
2025-05-07 08:12:26 +0200

    add fields to analysis

--- next.config.mjs
+++ next.config.mjs
@@ -15,5 +15,30 @@ export default withPWA({
   disable: process.env.NODE_ENV === "development",        // disable PWA in the development environment
   register: true,         // register the PWA service worker
   skipWaiting: true,      // skip waiting for service worker activation
-
+  swcMinify: true,
+  runtimeCaching: [ // each element is a caching strategy for different content 
+    {
+      urlPattern: /^https:\/\/your-api-domain\.com\/api/, //  Defines which URLs the caching rule applies to, can use callbacks or regex
+      handler: 'CacheOnly', // caching strategy 
+      options: {
+        cacheName: 'api-cache', // set a name for this caching storage 
+        expiration: { // set expiration for this cache and size
+          maxEntries: 32,
+          maxAgeSeconds: 24 * 60 * 60 // 24 hours
+        },
+      }
+    },
+    {
+      urlPattern: /\.(png|jpg|jpeg|svg|gif|webp)/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'image-cache',
+        plugins: []
+      }
+    },
+    // Add more route-specific caching strategies
+  ],
+  workboxOptions: {
+    disableDevLogs: true,
+  }
 })(nextConfig);
